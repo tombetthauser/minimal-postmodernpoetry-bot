3a7221f14a052388c735ced0136d7aebb91e1df3
tombetthauser <tombetthauser@gmail.com>
2021-06-05 21:34:28 -0700

    Finish up first version

--- poetry-bot.sh
+++ poetry-bot.sh
@@ -1,3 +1,7 @@
-perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
-perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
-echo ""
+while true; 
+  do 
+    perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
+    perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
+    echo
+    sleep 30
+  done
---
+++ update-script.sh
@@ -0,0 +1,3 @@
+# git add -A; git commit -m 'Update notes'; git push
+
+while true; do git add -A; git commit -m 'Update notes'; git push; sleep 1800; done
