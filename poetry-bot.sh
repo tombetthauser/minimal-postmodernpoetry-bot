2e3cee05941259a9f4a4604da2226cf4199fac4b
tombetthauser <tombetthauser@gmail.com>
2021-06-05 21:29:25 -0700

    get basics working

---
+++ poetry-bot.sh
@@ -0,0 +1,3 @@
+perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
+perl -e 'open IN, "</usr/share/dict/words";rand($.) < 1 && ($n=$_) while <IN>;print $n'
+echo ""
